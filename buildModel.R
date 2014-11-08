c91c233ddac999b84dab3b18b2a0a6edef33e0d8
Carsten Behring <carsten.behring@gmail.com>
2014-11-08 10:34:02 +0100

    using ngram instead of weka
    performance baseline for file  en_US.twitter.10.txt : 10 s on home PC

--- buildModel.R
+++ buildModel.R
@@ -2,6 +2,11 @@ library(tm)
 library(stringr)
 library(markovchain)
 library(ngram)
+library(slam)
+library(hash)
+
+
+
 
 ngram_tokenizer <- function(x) {
     text <- str_trim(stripWhitespace(as.character(x)))
@@ -15,14 +20,10 @@ ngram_tokenizer <- function(x) {
           ng3 <- get.ngrams(ngram(text,3))
       terms  <- str_trim(c(ng1,ng2,ng3))
       terms
-      #browser()
-      #print(words)
-
   }
 
 tokenizeFile <- function(filename) {
   file <- file(filename)
-  #vs <- VectorSource(readChar(file, file.info(filename)$size))
    vs <- VectorSource(readLines(file))
 
     corpus <- Corpus(vs)
@@ -31,13 +32,49 @@ tokenizeFile <- function(filename) {
    tdm <- TermDocumentMatrix(corpus, control = list(
                                         removePunctuation=T,
                                         removeNumbers=T,
-                                        tokenize = ngram_tokenizer
+                                         tokenize = ngram_tokenizer,
+                                         wordLengths=c(1,Inf)
    ))
   tdm$dimnames$Terms <- str_trim(tdm$dimnames$Terms)
   tdm
 }
 
-tdm <-tokenizeFile("en_US.twitter.100.txt")
-                                        #as.matrix(tdm)
 
-terms <- Terms(tdm)
+
+
+buildModel <- function(tdm) {
+
+    freqs <- row_sums(tdm)
+    terms <- str_trim(names(freqs))
+    name_pos <- hash(keys=terms,values = 1:length(terms))
+
+
+    dtcm <- simple_triplet_diag_matrix(rep(1,length(terms)))
+
+    for(term in terms) {
+
+        words <- MC_tokenizer(term)
+        l <- length(words)
+        if (l<2)
+            next
+
+
+        pos.last.blank <- tail(str_locate_all(term," ")[[1]],1)[1]
+
+        left <- str_trim(substring(term,pos.last.blank+1))
+        right <- str_trim(substring(term,1,pos.last.blank-1))
+
+        x <- values(name_pos,keys=right)
+        y <- values(name_pos,keys=left)
+        old <- as.matrix(dtcm[x,y])[1]
+        dtcm[x,y] <- old + 1
+       # print(paste(right,"-->",left,":",x,y,old+1))
+                                        #handleTerm(term,dtcm);
+    }
+    dtcm
+}
+
+
+
+#tdm <-tokenizeFile("en_US.twitter.100.txt")
+#dtcm <- buildModel(tdm)

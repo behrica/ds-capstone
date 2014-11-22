6c19ec6c6b72497cf61f5b15f9197acec5d78613
Carsten Behring <carsten.behring@gmail.com>
2014-11-22 21:39:30 +0000

    new file

---
+++ tune.R
@@ -0,0 +1,79 @@
+library(tm)
+library(stringr)
+library(stylo)
+library(quanteda)
+library(splitstackshape)
+
+numberOfSplits <- 100
+ngrams <- c(2,3,4,5)
+tdm.local.bound <- c(2,Inf)
+
+
+#---------------------
+
+splitsPerFile <- round(numberOfSplits / 3)
+
+
+my_tokenize <- function(x) {
+  cat(x$meta$id," / ",splitsPerFile,"\n")
+  terms <- c()
+  for (ngram in ngrams) {
+    ng <- make.ngrams(txt.to.words(x),ngram)
+    terms <- c(terms,ng)
+  }
+  terms
+}
+
+
+
+
+dirName <- paste0("f-",numberOfSplits)
+system(paste0("rm -rf /mnt/data/",dirName))
+
+system(paste0("mkdir /mnt/data/",dirName))
+
+system(paste0("cat /mnt/data/final/en_US/en_US.blogs.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/blogs"))
+system(paste0("cat /mnt/data/final/en_US/en_US.news.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/news"))
+system(paste0("cat /mnt/data/final/en_US/en_US.twitter.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/twitter"))
+
+options(mc.cores=1)
+textDir <- paste0("/mnt/data/",dirName)
+
+corpus <- Corpus(DirSource(textDir, encoding="UTF-8",pattern="*"), readerControl = list(language="en_US"))
+corpus <- tm_map(corpus,removePunctuation)
+corpus <- tm_map(corpus,removeNumbers)
+(f <- content_transformer(function(x) str_replace_all(x,"[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]", " ")))
+corpus <- tm_map(corpus, f)
+corpus <- tm_map(corpus,stripWhitespace)
+(g <- content_transformer(function(x) iconv(enc2utf8(x), sub = "byte")))
+corpus <- tm_map(corpus, g)
+corpus <- tm_map(corpus, content_transformer(tolower))
+
+options(mc.cores=7)
+tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize,tolower=F,bounds=list(local=tdm.local.bound)))
+counts <- row_sums(tdm)
+
+
+dt <- as.data.table(names(counts))
+dts <- cSplit(dt,"V1"," ")
+dts$count <- unname(counts)
+setnames(dts,c("t1","t2","t3","t4","count"))
+rm(dt,counts)
+
+
+
+predictNext <- function(tdm,term) {
+  matches <- grep(paste0(term," ([a-z])*"),Terms(tdm))
+  if (length(matches) == 0) {
+    words <- tokenize(term,simplify=T)
+    if (length(words) == 1 )
+      return("the")
+    predictNext(tdm,paste(words[-1],collapse=" " ))
+  }
+  
+  else  {
+    mostProbableWords <- sort(row_sums(tdm[matches,]),decreasing = T)
+    head(mostProbableWords)
+  }  
+}
+  
--- useTM.R
+++ useTM.R
@@ -4,6 +4,7 @@ library(stringr)
 library(slam)
 library(hash)
 library(stylo)
+library(slam)
 
 my_tokenize <- function(x,n=3) {
   text <- as.character(x)
@@ -21,9 +22,21 @@ my_tokenize_2 <- function(x) {
   
 }
 
+my_tokenize_3 <- function(x) {
+  ng3 <- make.ngrams(txt.to.words(x),3)
+  ng4 <- make.ngrams(txt.to.words(x),4)
+  ng5 <- make.ngrams(txt.to.words(x),5)
+ c(ng3,ng4,ng5)
+}
+
+my_tokenize_4 <- function(x) {
+  make.ngrams(txt.to.words(x),2)
+}
+
+
 
 #corpus <- readRDS("cleanCorpus.rds")
-options(mc.cores=8);tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize_2,tolower=F,bounds=list(local=c(2,Inf))))
+options(mc.cores=8);tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize_2,tolower=F,bounds=list(local=c(1,Inf))))
 terms <- row_sums(tdm)
 sum.hash <- hash(terms)
 
@@ -41,7 +54,27 @@ sum.hash <- hash(terms)
   sort(rowSums(as.matrix(tdm[matches,])),decreasing = T)[1]
 }
 
-# <<TermDocumentMatrix (terms: 4128659, documents: 100)  n=1,2,3,4   size =710M
-# <<TermDocumentMatrix (terms: 44905127, documents: 4272) n=3 size 4.4 GB
-# <<TermDocumentMatrix (terms: 333810, documents: 4272)>> n=3, bounds(2,Inf)
-#<<TermDocumentMatrix (terms: 482749, documents: 4272)>> n=3,4 bounds(2,Inf)
+# <<TermDocumentMatrix (terms: 4128659, documents: 100)  n=1,2,3,4   size =710M .local.bounds(1,Inf)  #docs = 4772
+# <<TermDocumentMatrix (terms: 44905127, documents: 4272) n=3 size 4.4 GB .local.bounds(1,Inf) #docs = 4772
+# <<TermDocumentMatrix (terms: 333810, documents: 4272)>> n=3, .local.bounds(2,Inf)  #docs = 4772
+# <<TermDocumentMatrix (terms: 482749, documents: 4272)>> n=3,4 .local.bounds(2,Inf)  #docs = 4772
+# <<TermDocumentMatrix (terms: 540961, documents: 4272)>>  n = 3,4,5 .local.bounds(2,Inf) #docs = 4772
+# <<TermDocumentMatrix (terms: 15743504, documents: 4272)>> n=2   .local.bounds(1,Inf) #docs = 4772
+# <<TermDocumentMatrix (terms: 51103022, documents: 99)>>   n=2,3 size=4.8 G .local. c(1,Inf) #docs = 100
+
+findTerms <- function(tdm,prefix) {
+  terms <- row_sums(tdm)
+  data.frame(count=sort(terms[grep(prefix,names(terms),value=F)],decreasing = T))
+}
+
+showLikelyHoods <- function(tdm,term,options) {
+  terms <- row_sums(tdm)
+  counts = c()
+  for (option in options) {
+    matches <-grep(paste0("^",term," ",option,"$"),names(terms),value=F)
+    print(paste(term,option,terms[matches]))
+    counts[[option]] <- terms[matches]
+  }
+  counts
+}
+  

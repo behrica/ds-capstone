a4a7b8089e1c9d09d4680ea4402d70bbb77dbb1a
Carsten Behring <carsten.behring@gmail.com>
2014-11-25 08:53:08 +0000

    print prob of phase working

--- tune.R
+++ tune.R
@@ -3,19 +3,23 @@ library(stringr)
 library(stylo)
 library(quanteda)
 library(splitstackshape)
+library(slam)
 
 numberOfSplits <- 100
-ngrams <- c(2,3,4,5)
+maxN <- 5
+ngrams <- c(1,2,3,4,maxN)
 tdm.local.bound <- c(2,Inf)
 
 
 #---------------------
 
 splitsPerFile <- round(numberOfSplits / 3)
+dirName <- paste0("f-",numberOfSplits)
+textDir <- paste0("/data/",dirName)
 
 
 my_tokenize <- function(x) {
-  cat(x$meta$id," / ",splitsPerFile,"\n")
+  message(x$meta$id," / ",splitsPerFile)
   terms <- c()
   for (ngram in ngrams) {
     ng <- make.ngrams(txt.to.words(x),ngram)
@@ -26,20 +30,14 @@ my_tokenize <- function(x) {
 
 
 
+system(paste0("rm -rf /data/",dirName))
+system(paste0("mkdir /data/",dirName))
+system(paste0("cat /data/final/en_US/en_US.blogs.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/blogs"))
+system(paste0("cat /data/final/en_US/en_US.news.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/news"))
+system(paste0("cat /data/final/en_US/en_US.twitter.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/twitter"))
 
-dirName <- paste0("f-",numberOfSplits)
-system(paste0("rm -rf /mnt/data/",dirName))
-
-system(paste0("mkdir /mnt/data/",dirName))
-
-system(paste0("cat /mnt/data/final/en_US/en_US.blogs.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/blogs"))
-system(paste0("cat /mnt/data/final/en_US/en_US.news.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/news"))
-system(paste0("cat /mnt/data/final/en_US/en_US.twitter.txt  | split -n r/",splitsPerFile,"  -d - /mnt/data/",dirName,"/twitter"))
-
-options(mc.cores=1)
-textDir <- paste0("/mnt/data/",dirName)
-
-corpus <- Corpus(DirSource(textDir, encoding="UTF-8",pattern="*"), readerControl = list(language="en_US"))
+options(mc.cores=4)
+corpus <- Corpus(DirSource(textDir, encoding="UTF-8",pattern="*00"), readerControl = list(language="en_US"))
 corpus <- tm_map(corpus,removePunctuation)
 corpus <- tm_map(corpus,removeNumbers)
 (f <- content_transformer(function(x) str_replace_all(x,"[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]", " ")))
@@ -49,25 +47,29 @@ corpus <- tm_map(corpus,stripWhitespace)
 corpus <- tm_map(corpus, g)
 corpus <- tm_map(corpus, content_transformer(tolower))
 
-options(mc.cores=7)
-tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize,tolower=F,bounds=list(local=tdm.local.bound)))
+options(mc.cores=4)
+tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize,tolower=F,bounds=list(local=tdm.local.bound),wordLengths=c(1,Inf)))
 counts <- row_sums(tdm)
 
 
-dt <- as.data.table(names(counts))
+dt <- data.table(names(counts))
 dts <- cSplit(dt,"V1"," ")
 dts$count <- unname(counts)
 setnames(dts,c("t1","t2","t3","t4","t5","count"))
 rm(dt,counts)
+
+for (i in seq_along(dts)) { 
+  set(dts, i=which(is.na(dts[[i]])), j=i, value="")
+}
 setkeyv(dts,c("t1","t2","t3","t4","t5"))
 
 # predict
-match <- (dts[list("i","want")])[is.na(t4)]
-match[order(count,decreasing = T)][2]
+#match <- (dts[list("i","want")])[is.na(t4)]
+#match[order(count,decreasing = T)][2]
 
 # predict
-match <- (dts[list("my","name","is")])[is.na(t5)]
-match[order(count,decreasing = T)][2]
+#match <- (dts[list("my","name","is")])[is.na(t5)]
+#match[order(count,decreasing = T)][2]
 
 
 
@@ -86,3 +88,34 @@ predictNext <- function(tdm,term) {
   }  
 }
   
+calcProbSingle <- function(dts,words) {
+  words.padded <- c(words,rep("",100))[1:maxN]
+  matched <- dts[as.list(words.padded),nomatch=0]
+  count.phrase <- matched$count
+  
+  words <- words[-1]
+  words.padded <- c(words,rep("",100))[1:maxN]
+  
+  
+  matched <- dts[as.list(words.padded),nomatch=0]
+  count.phrase_1 <- matched$count  
+  #if (is.na(count.phrase) | is.na(count.phrase_1)) 
+    #0
+  if (length(count.phrase) == 0 | length(count.phrase_1) == 0)
+    return(0)
+  count.phrase / count.phrase_1  
+}
+
+calcProb <- function(dts,phrase) {
+  words <- tokenize(phrase,simplify=T)
+  words <- words[max(1,(length(words)-maxN+1)):length(words)]
+  
+  while(TRUE) {
+    print(words)
+    prob <- calcProbSingle(dts,words)
+    words <- words[-1]
+    if (prob > 0 | length(words)==0)
+      return(prob)
+  }
+  
+}

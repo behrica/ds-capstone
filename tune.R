049ca88172500be039762c7a6664e1f722e031a0
Carsten Behring <carsten.behring@gmail.com>
2014-11-25 13:01:07 +0000

    split into functions

--- tune.R
+++ tune.R
@@ -9,7 +9,10 @@ numberOfSplits <- 100
 maxN <- 5
 ngrams <- c(1,2,3,4,maxN)
 tdm.local.bound <- c(2,Inf)
-
+column.names <- c()
+for (i in 1:maxN) {
+   column.names <- c(column.names,paste0("t",i))  
+}
 
 #---------------------
 
@@ -19,7 +22,8 @@ textDir <- paste0("/data/",dirName)
 
 
 my_tokenize <- function(x) {
-  message(x$meta$id," / ",splitsPerFile)
+  writeLines(paste(x$meta$id," / ",splitsPerFile),con)
+  flush(con)
   terms <- c()
   for (ngram in ngrams) {
     ng <- make.ngrams(txt.to.words(x),ngram)
@@ -28,64 +32,68 @@ my_tokenize <- function(x) {
   terms
 }
 
+#con <- file("progress.log","w")
+#close(con)
 
-
-system(paste0("rm -rf /data/",dirName))
-system(paste0("mkdir /data/",dirName))
-system(paste0("cat /data/final/en_US/en_US.blogs.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/blogs"))
-system(paste0("cat /data/final/en_US/en_US.news.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/news"))
-system(paste0("cat /data/final/en_US/en_US.twitter.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/twitter"))
-
-options(mc.cores=4)
-corpus <- Corpus(DirSource(textDir, encoding="UTF-8",pattern="*00"), readerControl = list(language="en_US"))
-corpus <- tm_map(corpus,removePunctuation)
-corpus <- tm_map(corpus,removeNumbers)
-(f <- content_transformer(function(x) str_replace_all(x,"[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]", " ")))
-corpus <- tm_map(corpus, f)
-corpus <- tm_map(corpus,stripWhitespace)
-(g <- content_transformer(function(x) iconv(enc2utf8(x), sub = "byte")))
-corpus <- tm_map(corpus, g)
-corpus <- tm_map(corpus, content_transformer(tolower))
-
-options(mc.cores=4)
-tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize,tolower=F,bounds=list(local=tdm.local.bound),wordLengths=c(1,Inf)))
-counts <- row_sums(tdm)
-
-
-dt <- data.table(names(counts))
-dts <- cSplit(dt,"V1"," ")
-dts$count <- unname(counts)
-setnames(dts,c("t1","t2","t3","t4","t5","count"))
-rm(dt,counts)
-
-for (i in seq_along(dts)) { 
-  set(dts, i=which(is.na(dts[[i]])), j=i, value="")
+splitFiles <- function() {
+  system(paste0("rm -rf /data/",dirName))
+  system(paste0("mkdir /data/",dirName))
+  system(paste0("cat /data/final/en_US/en_US.blogs.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/blogs"))
+  system(paste0("cat /data/final/en_US/en_US.news.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/news"))
+  system(paste0("cat /data/final/en_US/en_US.twitter.txt  | split -n r/",splitsPerFile,"  -d - /data/",dirName,"/twitter"))
 }
-setkeyv(dts,c("t1","t2","t3","t4","t5"))
 
-# predict
-#match <- (dts[list("i","want")])[is.na(t4)]
-#match[order(count,decreasing = T)][2]
+createCorpus <- function(pattern="*") {
+  options(mc.cores=4)
+  corpus <- Corpus(DirSource(textDir, encoding="UTF-8",pattern=pattern), readerControl = list(language="en_US"))
+  corpus <- tm_map(corpus,removePunctuation)
+  corpus <- tm_map(corpus,removeNumbers)
+  (f <- content_transformer(function(x) str_replace_all(x,"[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]", " ")))
+  corpus <- tm_map(corpus, f)
+  corpus <- tm_map(corpus,stripWhitespace)
+  (g <- content_transformer(function(x) iconv(enc2utf8(x), sub = "byte")))
+  corpus <- tm_map(corpus, g)
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus   
+}
 
-# predict
-#match <- (dts[list("my","name","is")])[is.na(t5)]
-#match[order(count,decreasing = T)][2]
+
+createTdm <- function(corpus) {
+  options(mc.cores=4)
+  tdm <- TermDocumentMatrix(corpus,control=list(tokenize=my_tokenize,tolower=F,bounds=list(local=tdm.local.bound),wordLengths=c(1,Inf)))
+  tdm  
+}
 
 
-
-predictNext <- function(tdm,term) {
-  matches <- grep(paste0(term," ([a-z])*"),Terms(tdm))
-  if (length(matches) == 0) {
-    words <- tokenize(term,simplify=T)
-    if (length(words) == 1 )
-      return("the")
-    predictNext(tdm,paste(words[-1],collapse=" " ))
-  }
+createDT <- function(tdm) {
+  counts <- row_sums(tdm)
+  dt <- data.table(names(counts))
+  dts <- cSplit(dt,"V1"," ")
+  dts$count <- unname(counts)
+  setnames(dts,c("t1","t2","t3","t4","t5","count"))
+  rm(dt,counts)
   
-  else  {
-    mostProbableWords <- sort(row_sums(tdm[matches,]),decreasing = T)
-    head(mostProbableWords)
-  }  
+  for (i in seq_along(dts)) { 
+    set(dts, i=which(is.na(dts[[i]])), j=i, value="")
+  }
+  setkeyv(dts,c("t1","t2","t3","t4","t5"))
+  dts
+}
+
+predictNextFromWords <- function(dts,words) {
+  by <- column.names[min(maxN,length(words)+1)]
+  allMatches <- dts[as.list(words),j=sum(count),by=by]
+  allMatches <- allMatches[ allMatches[[by]] !="" ,]
+  predictedWord <- as.character(allMatches[order(V1,decreasing = T)][2][,by,with=FALSE][[1]])
+  if (is.na(predictedWord)) {
+    predictedWord <- predictNextFromWords(dts,words[-1])
+  }
+  predictedWord
+}
+
+predictNext <- function(dts,phrase) {
+  words <- tokenize(phrase,simplify=T)
+  predictNextFromWords(dts,words)
 }
   
 calcProbSingle <- function(dts,words) {
